795cee4acc1df403ab4be63b33e8dc10ccb37137
jakeee51 <jakemorfe@gmail.com>
2020-03-16 23:21:01 -0400

    Initialize

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import paramiko
 
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect('192.168.10.204', username='jake', password='mantabayray51')
+
+stdin, stdout, stderr = client.exec_command('cd /home/jake')
+stdin, stdout, stderr = client.exec_command('./test.sh')
+
+
+for line in stdout:
+    print(line.strip('\n'))
+
+client.close()
